27f043465022fcc362564ddf0c6f8c809341ac29
Olivia Croteau <oliviacroteau667@gmail.com>
2024-11-14 19:06:47 -0500

    initial commit
    psuedo-code written to outline program

---
+++ practicingSoftwareDevelopment/practicingSoftwareDevelopment/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace practicingSoftwareDevelopment
+//Dictionary - Phone Book
+
+    /*
+     * Goals:
+     * Make a phone book
+     * Add person with number
+     * Delete person by name
+     * Display entire phone book
+     * Search phone book by name and return result
+     */
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare dictionary for phoneBook
+            var phoneBook = new Dictionary<string, string>
+        {
+            { "key1", "value1" },
+            { "key2", "value2" }
+        };
+
+            Console.WriteLine(phoneBook["key1"]);
+        }
+    }
+}
