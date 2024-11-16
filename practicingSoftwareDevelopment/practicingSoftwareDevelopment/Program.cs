ed2902635e0f036a0d2fa0d4e526b09a3e302500
Olivia Croteau <oliviacroteau667@gmail.com>
2024-11-16 10:51:50 -0500

    Updated Program.cs
    For some reason our original comments didn't push the first time. Repo is now up to date.

--- practicingSoftwareDevelopment/practicingSoftwareDevelopment/Program.cs
+++ practicingSoftwareDevelopment/practicingSoftwareDevelopment/Program.cs
@@ -1,14 +1,18 @@
-﻿namespace practicingSoftwareDevelopment
+﻿using System.Numerics;
+using System.Xml.Linq;
+using System;
+
+namespace practicingSoftwareDevelopment
 //Dictionary - Phone Book
 
-    /*
-     * Goals:
-     * Make a phone book
-     * Add person with number
-     * Delete person by name
-     * Display entire phone book
-     * Search phone book by name and return result
-     */
+/*
+ * Goals:
+ * Make a phone book
+ * Add person with number
+ * Delete person by name
+ * Display entire phone book
+ * Search phone book by name and return result
+ */
 {
     internal class Program
     {
@@ -17,11 +21,37 @@
             //declare dictionary for phoneBook
             var phoneBook = new Dictionary<string, string>
         {
-            { "key1", "value1" },
+            { "Olivia Croteau", "(518) 925-9554" },
             { "key2", "value2" }
-        };
 
-            Console.WriteLine(phoneBook["key1"]);
+            //code block to determine which function to call --> Olivia
+            //take user input to determine which function to take
+            //use switch case. options:
+            //Add person with number
+            //Delete person by name
+            //Display entire phone book
+            //Search phone book by name and return result
+        };
         }
+
+        //Function to addPerson() --> Mariana
+        //taking user input for each field and assign to key value pairs
+        //dictionary.Add() check if person already exists in dictionary. display confirmation message
+        //if person does not exist, display message
+
+        //Function  to deletePerson() --> Mariana
+        //take user input for person to be removed
+        //dictionary.Remove() using name as key value. display confirmation message
+        //if person does not exist, display message
+
+        //Function to displayPhoneBook() --> Ella
+        //display a title
+        //for loop to iterate through phoneBook
+        //log the name (key)
+        //log phone number at that name (value)
+
+        //Function to searchPhoneBook() --> Jacob
+        //take user input for key
+        //display phone number at that key
     }
 }
