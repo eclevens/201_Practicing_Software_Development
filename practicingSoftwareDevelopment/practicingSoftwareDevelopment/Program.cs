7cd0136caaf8e93e1c9364ba3b22042c2748b15e
Olivia Croteau <oliviacroteau667@gmail.com>
2024-11-17 12:44:48 -0500

    Final check of program. Updated comment blocks with notes based on rubric.

--- practicingSoftwareDevelopment/practicingSoftwareDevelopment/Program.cs
+++ practicingSoftwareDevelopment/practicingSoftwareDevelopment/Program.cs
@@ -9,11 +9,18 @@ namespace practicingSoftwareDevelopment
 
 /*
  * Goals:
- * Make a phone book
+ * Make a phone book that can handle user input.
  * Add person with number
  * Delete person by name
  * Display entire phone book
  * Search phone book by name and return result
+
+    The above goals represent our brainstorming (we didn't make a seperate document since the info needed to be most readily available here.
+    Brainstorming was done as a team to determine these goals.
+    Olivia: Build Main() code for phone book and helper functions to handle user input and call the rest of the program.
+    Mariana: Build functions to add and delete contacts.
+    Ella: Build function to display the entire phone book.
+    Jacob: Build function to search the phone book for a specific contact.
  */
 {
     internal class Program
@@ -50,7 +57,7 @@ namespace practicingSoftwareDevelopment
             //Delete person by name
             //Display entire phone book
             //Search phone book by name and return result
-        static void Switcher(Dictionary<string, string> PhoneBook)
+        static void Switcher(Dictionary<string, string> PhoneBook) 
         {
             string userChoice = Console.ReadLine();
 
